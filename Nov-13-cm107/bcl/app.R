d0b738961c861533b9d66027928d46d3d5b33f25
Rachel Lobay <rachel.lobay@stat.ubc.ca>
2018-11-13 10:45:48 -0800

    Final in-class push of cm107 shiny app.

--- Nov-13-cm107/bcl/app.R
+++ Nov-13-cm107/bcl/app.R
@@ -9,26 +9,38 @@
 
 library(shiny)
 
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
 # Anything before UI will be run too!
-a <- 5
-print(a^2)
+#a <- 5
+# print(a^2)
 
 
 # UI (User Interface) output of fluid page function 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  "This is some text",
-  p("This is more text."),
+ # "This is some text",
+ # p("This is more text."),
   # h1 is level 1 header
-  tags$h1("Level 1 header"),
-  h1(em("Level 1 header")),
-  HTML("<h1>Level 1 header, part 3 </h1>"),
-  "Some text following a break",
-  code("This text will be displayed as computer code."),
-  br(),
-  a(href="www.rstudio.com", "Click here!"),
-  br(),
-  print(a)
+ # tags$h1("Level 1 header"),
+ # h1(em("Level 1 header")),
+ # HTML("<h1>Level 1 header, part 3 </h1>"),
+ # "Some text following a break",
+ # code("This text will be displayed as computer code."),
+ # br(),
+ # a(href="www.rstudio.com", "Click here!"),
+ # br(),
+ # print(a)
+  
+  titlePanel("BC Liquor price app", 
+             windowTitle = "BCL app"),
+  sidebarLayout(
+    sidebarPanel("This text is in the sidebar."),
+    mainPanel(
+      plotOutput("price_hist"),
+      tableOutput("bcl_data")
+    )
+  )
 )
   
 
@@ -37,7 +49,8 @@ ui <- fluidPage(
 # Server function is a function of input and output
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+   output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
+   output$bcl_data <- renderTable(bcl)
 
 }
 
